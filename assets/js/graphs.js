42331c11edbd2a073b9a1ee5132c1d3d2c2d8f5a
Ubuntu <ubuntu@ip-172-31-88-1.ec2.internal>
2019-08-02 19:09:05 +0000

    Added stacked area chart

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -9,10 +9,11 @@ function makeGraphs(dataset) {
     dataset.forEach(function(d) {
         // convert Units from string to integer
         d.Units = parseInt(d.Units);
-        d.Year = new Date(d.Year);
+        //d.Year = new Date(d.Year);
     });
 
-    mybarplot(dataset.filter(function (d) { return d.Year.getFullYear() == 2019; }));
+    //mybarplot(dataset.filter(function (d) { return d.Year.getFullYear() == 2019; }));
+    mybarplot(dataset.filter(function (d) { return d.Year == "2019"; }));
     mystackedareaplot(dataset);
     //mytreemap(data);
     
@@ -75,7 +76,7 @@ function mybarplot(dataset) {
        .call(d3.axisLeft(yScale));
 
     // append the bars
-    svg.selectAll()
+    svg.selectAll("rect")
        .data(dataset)
        .enter()
        .append("rect")
@@ -94,6 +95,9 @@ function mybarplot(dataset) {
 }
 
 function mystackedareaplot(dataset) {
+    // https://www.d3-graph-gallery.com/graph/stackedarea_basic.html
+    // https://medium.com/@louisemoxy/how-to-create-a-stacked-area-chart-with-d3-28a2fee0b8ca
+    
     const margin = {top: 30, right: 30, bottom: 70, left: 60};
     const width = 460 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
@@ -105,20 +109,66 @@ function mystackedareaplot(dataset) {
                   .append("g")
                   .attr("transform",`translate(${margin.left},${margin.top})`);
     
+    // sort data
+    dataset.sort(function(a, b) {
+         return parseInt(a.Year) - parseInt(b.Year);
+    });
+    
     // group the data: one array for each value of the X axis.
     const sumstat = d3.nest()
                       .key(function(d) { return d.Year; })
                       .entries(dataset);
 
-    console.log(sumstat);
-       // var mygroups = ["Helen", "Amanda", "Ashley"] // list of group names
- //    var mygroup = [1,2,3] // list of group names
-//    var stackedData = d3.stack()
-    //.keys(mygroup)
-    //.value(function(d, key){
-     // return d.values[key].n
-    //})
-    //(sumstat)
+    //console.log(sumstat);
+    
+    const mygroups = ["VOLKSWAGEN", "TOYOTA", "HYUNDAI"] // list of group names
+    
+    const mygroup = [1,2,3] // list of group names
+  
+    const stackedData = d3.stack()
+                          .keys(mygroup)
+                          .value(function(d, key){ 
+                                    return d.values[key].Units
+                                })(sumstat);
+
+    //console.log(stackedData);
+    
+    // x-axis
+    const x = d3.scaleLinear()
+                .domain(d3.extent(dataset, function(d) { return d.Year; }))
+                .range([ 0, width ]);
+    
+    svg.append("g")
+       .attr("transform", "translate(0," + height + ")")
+       .call(d3.axisBottom(x).ticks(5));
+    
+    // y-axis
+    const y = d3.scaleLinear()
+                //.domain([0, d3.max(dataset, function(d) { return d.Units; })*1.2])
+                .domain([0,50000])
+                .range([ height, 0 ]);
+    
+    svg.append("g")
+       .call(d3.axisLeft(y));
+       
+    const color = d3.scaleOrdinal()
+                  .domain(mygroups)
+                  .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']);
+                  
+    // Show the areas
+    svg
+         .selectAll("mylayers")
+         .data(stackedData)
+         .enter()
+         .append("path")
+         .style("fill", function(d) { name = mygroups[d.key-1] ;  return color(name); })
+         .attr("d", d3.area()
+         .x(function(d, i) { return x(d.data.key); })
+         .y0(function(d) { return y(d[0]); })
+         .y1(function(d) { return y(d[1]); })
+     );
+
+
 }
 
 // function mytreemap(data) {
