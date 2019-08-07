f20c1e21e13dec75f00a09da61cc87f9b716868a
Ubuntu <ubuntu@ip-172-31-88-1.ec2.internal>
2019-08-07 18:54:44 +0000

    Worked on treemap function

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -57,6 +57,7 @@ function makeGraphs2(dataset) {
 }
 
 function makeGraphs3(dataset) {
+    
     // UnitsByYearMakeModel
     console.log(dataset.name);
     // 2019
@@ -67,6 +68,8 @@ function makeGraphs3(dataset) {
     console.log(dataset.children[0].children[0].children[0].name);
     // 267
     console.log(dataset.children[0].children[0].children[0].value);
+    
+    mytreemap(dataset.children[0].children[0]);
 }
 
 //=================================================
@@ -450,41 +453,41 @@ function mystackedareaplot(dataset) {
 }
 
 function mytreemap(dataset) {
-    // https://www.d3-graph-gallery.com/graph/treemap_json.html
+    // https://www.d3-graph-gallery.com/graph/treemap_custom.html
 
-      // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 10, bottom: 10, left: 10},
-//   width = 445 - margin.left - margin.right,
-//   height = 445 - margin.top - margin.bottom;
+    // set the dimensions and margins of the graph
+    const margin = {top: 10, right: 10, bottom: 10, left: 10};
+    const width = 445 - margin.left - margin.right;
+    const height = 445 - margin.top - margin.bottom;
 
-// // append the svg object to the body of the page
-// var svg = d3.select("#my_dataviz")
-// .append("svg")
-//   .attr("width", width + margin.left + margin.right)
-//   .attr("height", height + margin.top + margin.bottom)
-// .append("g")
-//   .attr("transform",
-//         "translate(" + margin.left + "," + margin.top + ")");
+    // append the svg object to the body of the page
+    const svg = d3.select("#mytreemap")
+                  .append("svg")
+                  .attr("width", width + margin.left + margin.right)
+                  .attr("height", height + margin.top + margin.bottom)
+                  .append("g")
+                  .attr("transform",`translate(${margin.left},${margin.top})`);
 
-// // read json data
-// d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_dendrogram_full.json", function(data) {
+    // Give the data to this cluster layout
+    // Here the size of each leave is given in the 'value' field in input data
+    const root = d3.hierarchy(dataset)
+                   .sum(function(d) { return d.value; });
+    
+    //console.log(root.leaves());
+    
+    // Then d3.treemap computes the position of each element of the hierarchy
+    d3.treemap()
+      .size([width, height])
+      .paddingTop(28)
+      .paddingRight(7)
+      .paddingInner(3)      // Padding between each rectangle
+      //.paddingOuter(6)
+      //.padding(20)
+      (root);
 
-//   // Give the data to this cluster layout:
-//   var root = d3.hierarchy(data).sum(function(d){ return d.value}) // Here the size of each leave is given in the 'value' field in input data
-
-//   // Then d3.treemap computes the position of each element of the hierarchy
-//   d3.treemap()
-//     .size([width, height])
-//     .paddingTop(28)
-//     .paddingRight(7)
-//     .paddingInner(3)      // Padding between each rectangle
-//     //.paddingOuter(6)
-//     //.padding(20)
-//     (root)
-
-//   // prepare a color scale
-//   var color = d3.scaleOrdinal()
-//     .domain(["boss1", "boss2", "boss3"])
+//    // prepare a color scale
+//    const color = d3.scaleOrdinal()
+//    .domain(["boss1", "boss2", "boss3"])
 //     .range([ "#402D54", "#D18975", "#8FD175"])
 
 //   // And a opacity scale
@@ -492,31 +495,29 @@ function mytreemap(dataset) {
 //     .domain([10, 30])
 //     .range([.5,1])
 
-//   // use this information to add rectangles:
-//   svg
-//     .selectAll("rect")
-//     .data(root.leaves())
-//     .enter()
-//     .append("rect")
-//       .attr('x', function (d) { return d.x0; })
-//       .attr('y', function (d) { return d.y0; })
-//       .attr('width', function (d) { return d.x1 - d.x0; })
-//       .attr('height', function (d) { return d.y1 - d.y0; })
-//       .style("stroke", "black")
+    // use this information to add rectangles:
+    svg.selectAll("rect")
+       .data(root.leaves())
+       .enter()
+       .append("rect")
+       .attr('x', function (d) { return d.x0; })
+       .attr('y', function (d) { return d.y0; })
+       .attr('width', function (d) { return d.x1 - d.x0; })
+       .attr('height', function (d) { return d.y1 - d.y0; })
+       .style("stroke", "black");
 //       .style("fill", function(d){ return color(d.parent.data.name)} )
 //       .style("opacity", function(d){ return opacity(d.data.value)})
 
-//   // and to add the text labels
-//   svg
-//     .selectAll("text")
-//     .data(root.leaves())
-//     .enter()
-//     .append("text")
-//       .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
-//       .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
-//       .text(function(d){ return d.data.name.replace('mister_','') })
-//       .attr("font-size", "19px")
-//       .attr("fill", "white")
+    // // and to add the text labels
+    // svg.selectAll("text")
+    //   .data(root.leaves())
+    //   .enter()
+    //   .append("text")
+    //   .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
+    //   .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
+    //   .text(function(d){ return d.data.name.replace('mister_','') })
+    //   .attr("font-size", "19px")
+    //   .attr("fill", "white");
 
 //   // and to add the text labels
 //   svg
