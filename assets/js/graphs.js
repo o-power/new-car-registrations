b2d61d88e6346646ad7de38400a2de9be22522c5
Ubuntu <ubuntu@ip-172-31-88-1.ec2.internal>
2019-08-07 19:26:02 +0000

    Added labels to treemap

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -68,6 +68,8 @@ function makeGraphs3(dataset) {
     console.log(dataset.children[0].children[0].children[0].name);
     // 267
     console.log(dataset.children[0].children[0].children[0].value);
+    //
+    console.log(dataset.children[0].children[0]);
     
     mytreemap(dataset.children[0].children[0]);
 }
@@ -454,6 +456,7 @@ function mystackedareaplot(dataset) {
 
 function mytreemap(dataset) {
     // https://www.d3-graph-gallery.com/graph/treemap_custom.html
+    // http://bl.ocks.org/JacquesJahnichen/42afd0cde7cbf72ecb81
 
     // set the dimensions and margins of the graph
     const margin = {top: 10, right: 10, bottom: 10, left: 10};
@@ -478,23 +481,32 @@ function mytreemap(dataset) {
     // Then d3.treemap computes the position of each element of the hierarchy
     d3.treemap()
       .size([width, height])
-      .paddingTop(28)
-      .paddingRight(7)
+      //.paddingTop(28)
+      //.paddingRight(7)
       .paddingInner(3)      // Padding between each rectangle
       //.paddingOuter(6)
       //.padding(20)
       (root);
+      
+    // scales
+    // https://www.d3-graph-gallery.com/graph/custom_axis.html
 
-//    // prepare a color scale
-//    const color = d3.scaleOrdinal()
-//    .domain(["boss1", "boss2", "boss3"])
-//     .range([ "#402D54", "#D18975", "#8FD175"])
+    // prepare a color scale
+    // One color per group, opacity proportional to value: classic use of scale
+    const color = d3.scaleOrdinal()
+                    .domain(["VOLVO"])
+                    .range([ "#402D54"]);
 
-//   // And a opacity scale
-//   var opacity = d3.scaleLinear()
-//     .domain([10, 30])
-//     .range([.5,1])
+    // And a opacity scale
+    const opacity = d3.scaleLinear()
+                    .domain(d3.extent(dataset.children.map(function(child) { 
+                      return +child.value;
+                    })))
+                    .range([.5,1]);
 
+    
+    console.log(root.leaves());
+    
     // use this information to add rectangles:
     svg.selectAll("rect")
        .data(root.leaves())
@@ -504,38 +516,36 @@ function mytreemap(dataset) {
        .attr('y', function (d) { return d.y0; })
        .attr('width', function (d) { return d.x1 - d.x0; })
        .attr('height', function (d) { return d.y1 - d.y0; })
-       .style("stroke", "black");
-//       .style("fill", function(d){ return color(d.parent.data.name)} )
-//       .style("opacity", function(d){ return opacity(d.data.value)})
+       .style("stroke", "black")
+       .style("fill", function(d) { return color(d.parent.name); } )
+       .style("opacity", function(d) { return opacity(d.data.value); });
 
-    // // and to add the text labels
-    // svg.selectAll("text")
-    //   .data(root.leaves())
-    //   .enter()
-    //   .append("text")
-    //   .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
-    //   .attr("y", function(d){ return d.y0+20})    // +20 to adjust position (lower)
-    //   .text(function(d){ return d.data.name.replace('mister_','') })
-    //   .attr("font-size", "19px")
-    //   .attr("fill", "white");
+    // and to add the text labels
+    svg.selectAll("text")
+       .data(root.leaves())
+       .enter()
+       .append("text")
+       .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
+       .attr("y", function(d){ return d.y0+20; })    // +20 to adjust position (lower)
+       .text(function(d) { return d.data.name; })
+       .attr("font-size", "12px")
+       .attr("fill", "white");
 
-//   // and to add the text labels
-//   svg
-//     .selectAll("vals")
-//     .data(root.leaves())
-//     .enter()
-//     .append("text")
-//       .attr("x", function(d){ return d.x0+5})    // +10 to adjust position (more right)
-//       .attr("y", function(d){ return d.y0+35})    // +20 to adjust position (lower)
-//       .text(function(d){ return d.data.value })
-//       .attr("font-size", "11px")
-//       .attr("fill", "white")
+    // and to add the text labels
+    svg.selectAll("vals")
+       .data(root.leaves())
+       .enter()
+       .append("text")
+       .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
+       .attr("y", function(d){ return d.y0+35; })    // +20 to adjust position (lower)
+       .text(function(d){ return d.data.value; })
+       .attr("font-size", "11px")
+       .attr("fill", "white")
 
-//   // Add title for the 3 groups
-//   svg
-//     .selectAll("titles")
-//     .data(root.descendants().filter(function(d){return d.depth==1}))
-//     .enter()
+    // Add title for the 3 groups
+//    svg.selectAll("titles")
+ //      .data(root.descendants().filter(function(d){return d.depth==1}))
+//       .enter()
 //     .append("text")
 //       .attr("x", function(d){ return d.x0})
 //       .attr("y", function(d){ return d.y0+21})
