106cf99292ceadaa3d70ac006e33d915fad08d54
Ubuntu <ubuntu@ip-172-31-88-1.ec2.internal>
2019-08-15 21:39:41 +0000

    Added treemap

--- assets/js/graphs.js
+++ assets/js/graphs.js
@@ -4,11 +4,11 @@
 
 Promise.all([
     d3.csv("assets/data/UnitsByYear.csv"),
-    d3.csv("assets/data/UnitsByColourAndYear.csv")
+    d3.csv("assets/data/UnitsByColourAndYear.csv"),
+    d3.json("assets/data/UnitsByMakeModelAndYear.json")
     //d3.csv("assets/data/UnitsByMakeAndYear.csv"),
-    //d3.json("assets/data/UnitsByMakeModelAndYear.json")
 ]).then(function(files) {
-    makeGraphs(files[0], files[1]);
+    makeGraphs(files[0], files[1], files[2]);
     //makeGraphs11(files[1]);
     //makeGraphs2(files[2]);
     //makeGraphs3(files[3]);
@@ -20,7 +20,7 @@ Promise.all([
   * makeGraphs()
   * 
   */
-function makeGraphs(unitsByYear, unitsByColourAndYear) {
+function makeGraphs(unitsByYear, unitsByColourAndYear, unitsByMakeModelAndYear) {
     
     unitsByYear.forEach(function(d) {
         d.Units = parseInt(d.Units);
@@ -35,6 +35,7 @@ function makeGraphs(unitsByYear, unitsByColourAndYear) {
     
     newVsOldStackedAreaChart(unitsByYear);
     colourBumpChart(unitsByColourAndYear);
+    makeModelTreemap(unitsByMakeModelAndYear.children[0]);
 }
 
 /**
@@ -413,288 +414,11 @@ function colourBumpChart(dataset) {
          })
 }
 
-/*******************************************************************/
 /**
-  * makeGraphs11()
+  * makeModelTreeemap()
   * 
   */
-function makeGraphs11(dataset) {
-    
-    dataset.forEach(function(d) {
-        d.Units = parseInt(d.Units);
-    });
-
-    mybarplot(dataset.filter(function (d) { return d.Year == "2019"; }));
-    mystackedareaplot(dataset);
-    
-    // {Rank: "1", Make: "TOYOTA", Units: 21724, Year: "2008"}
-    //console.log(dataset[0]);
-    
-    // "1"
-    //console.log(dataset[0].Rank);
-    // "TOYOTA"
-    //console.log(dataset[0].Make);
-    // 21724
-    //console.log(dataset[0].Units);
-    // "2008"
-    //console.log(dataset[0].Year);
-    // 489
-    //console.log(dataset.length);
-}
-
-/**
-  * makeGraphs2()
-  * 
-  */
-function makeGraphs2(dataset) {
-    
-    dataset.forEach(function(d) {
-        d.Units = parseInt(d.Units);
-        d.Rank = parseInt(d.Rank);
-        d.Year = parseInt(d.Year);
-    });
-    
-    mybumpchart(dataset.filter(function (d) { return (d.Year > 2009); }));
-    
-    // {Colour: "Grey", Units: 30023, Year: 2019, Rank: 1, Class: "grey"}
-    //console.log(dataset[0]);
-}
-
-/**
-  * makeGraphs3()
-  * 
-  */
-function makeGraphs3(dataset) {
-    
-    // {children: Array(12), name: "UnitsByYearMakeModel"}
-    //console.log(dataset);
-    
-    // UnitsByYearMakeModel
-    //console.log(dataset.name);
-    
-    // {children: Array(35), name: "2019"}
-    //console.log(dataset.children[0]);
-    
-    // "2019"
-    //console.log(dataset.children[0].name);
-    
-    // {children: Array(11), name: "VOLVO"}
-    //console.log(dataset.children[0].children[0]);
-    
-    // "VOLVO"
-    //console.log(dataset.children[0].children[0].name);
-    
-    // {name: "XC60", value: 267}
-    //console.log(dataset.children[0].children[0].children[0]);
-    
-    // "XC60"
-    //console.log(dataset.children[0].children[0].children[0].name);
-    // 267
-    //console.log(dataset.children[0].children[0].children[0].value);
-    
-    mytreemap(dataset.children[0]);
-}
-
-/**
-  * mybarplot()
-  * 
-  */
-function mybarplot(dataset) {
-    
-    // References:
-    // https://www.d3-graph-gallery.com/graph/barplot_ordered.html
-    // https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
-    
-    // set the dimensions and margins of the graph
-    const margin = {top: 30, right: 30, bottom: 70, left: 60};
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-
-    // append the svg object to the div
-    // add margins
-    const svg = d3.select("#mybarplot")
-                  .append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform",`translate(${margin.left},${margin.top})`);
-    
-    // // sort data
-    // data.sort(function(b, a) {
-    //     return a.Value - b.Value;
-    // });
-    
-    // x-axis scale
-    var xScale = d3.scaleBand()
-                   .domain(dataset.map(function(d) { return d.Make; }))
-                   .range([0, width])
-                   .padding(0.2);
-    
-    // append x-axis to the svg object
-    // translate moves axis to bottom of chart
-    // this means the axis is actually in the bottom margin
-    svg.append("g")
-       .attr("transform", `translate(0, ${height})`)
-       .call(d3.axisBottom(xScale))
-       .selectAll("text")
-       .attr("transform", "translate(-10,0)rotate(-45)")
-       .style("text-anchor", "end");
-
-    // y-axis scale
-    var yScale = d3.scaleLinear()
-                   .domain([0, d3.max(dataset, function(d) { return d.Units; })])
-                   .range([height, 0]);
-    
-    // append y-axis to the svg object
-    svg.append("g")
-       .call(d3.axisLeft(yScale));
-
-    // append the bars
-    svg.selectAll("rect")
-       .data(dataset)
-       .enter()
-       .append("rect")
-       .attr("x", function(d) { return xScale(d.Make); })
-       .attr("y", function(d) { return yScale(d.Units); })
-       .attr("width", xScale.bandwidth())
-       .attr("height", function(d) { return height - yScale(d.Units); })
-       .attr("fill", "#69b3a2");
-    
-    // add gridlines https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
-    // svg.append('g')
-    //   .attr('class', 'grid')
-    //   .attr('transform', `translate(0, ${height})`)
-    //   .call(d3.axisBottom()
-    //   .scale(xScale)
-    //   .tickSize(-height, 0, 0)
-    //   .tickFormat(''))
-    
-    svg.append('g')
-       .attr('class', 'grid')
-       .call(d3.axisLeft()
-       .scale(yScale)
-       .tickSize(-width, 0, 0)
-       .tickFormat(''));
-       
-    // add y label
-    svg.append('text')
-       .attr('x', -height/2)
-       .attr('y', -margin.left/2)
-       .attr('transform', 'rotate(-90)')
-       .attr('text-anchor', 'middle')
-       .text('Units');
-    
-    // add title
-    svg.append('text')
-       .attr('x', width / 2 + margin.left)
-       .attr('y', margin.top/2)
-       .attr('text-anchor', 'middle')
-       .text('Car makes by units (2019)');
-    
-    // add interactivity
-    svg.selectAll("rect")
-       .on('mouseenter', function (actual, i) {
-           console.log(this);
-           d3.select(this).attr('opacity', 0.5);
-       })
-       .on('mouseleave', function (actual, i) {
-           d3.select(this).attr('opacity', 1);
-       });
-    
-    // add colors and fonts
-    
-    
-    // https://blog.risingstack.com/tutorial-d3-js-calendar-heatmap/
-    // https://jsfiddle.net/matehu/w7h81xz2/
-}
-
-/**
-  * mystackedareaplot()
-  * 
-  */
-function mystackedareaplot(dataset) {
-    // https://www.d3-graph-gallery.com/graph/stackedarea_basic.html
-    // https://medium.com/@louisemoxy/how-to-create-a-stacked-area-chart-with-d3-28a2fee0b8ca
-    
-    const margin = {top: 30, right: 30, bottom: 70, left: 60};
-    const width = 460 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
-
-    const svg = d3.select("#mystackedareaplot")
-                  .append("svg")
-                  .attr("width", width + margin.left + margin.right)
-                  .attr("height", height + margin.top + margin.bottom)
-                  .append("g")
-                  .attr("transform",`translate(${margin.left},${margin.top})`);
-    
-    // sort data
-    dataset.sort(function(a, b) {
-         return parseInt(a.Year) - parseInt(b.Year);
-    });
-    
-    // group the data: one array for each value of the X axis.
-    const sumstat = d3.nest()
-                      .key(function(d) { return d.Year; })
-                      .entries(dataset);
-
-    
-    //console.log(sumstat);
-    
-    const mygroups = ["VOLKSWAGEN", "TOYOTA", "HYUNDAI"] // list of group names
-    
-    const mygroup = [1,2,3] // list of group names
-  
-    const stackedData = d3.stack()
-                          .keys(mygroup)
-                          .value(function(d, key){ 
-                                    return d.values[key].Units
-                                })(sumstat);
-
-    //console.log(stackedData);
-    
-    // x-axis
-    const x = d3.scaleLinear()
-                .domain(d3.extent(dataset, function(d) { return d.Year; }))
-                .range([ 0, width ]);
-    
-    svg.append("g")
-       .attr("transform", "translate(0," + height + ")")
-       .call(d3.axisBottom(x).ticks(5));
-    
-    // y-axis
-    const y = d3.scaleLinear()
-                //.domain([0, d3.max(dataset, function(d) { return d.Units; })*1.2])
-                .domain([0,50000])
-                .range([ height, 0 ]);
-    
-    svg.append("g")
-       .call(d3.axisLeft(y));
-       
-    const color = d3.scaleOrdinal()
-                  .domain(mygroups)
-                  .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']);
-                  
-    // Show the areas
-    svg
-         .selectAll("mylayers")
-         .data(stackedData)
-         .enter()
-         .append("path")
-         .style("fill", function(d) { name = mygroups[d.key-1] ;  return color(name); })
-         .attr("d", d3.area()
-         .x(function(d, i) { return x(d.data.key); })
-         .y0(function(d) { return y(d[0]); })
-         .y1(function(d) { return y(d[1]); })
-     );
-
-
-}
-
-/**
-  * mytreemap()
-  * 
-  */
-function mytreemap(dataset) {
+function makeModelTreemap(dataset) {
     // https://www.d3-graph-gallery.com/graph/treemap_custom.html
     // http://bl.ocks.org/JacquesJahnichen/42afd0cde7cbf72ecb81
 
@@ -704,7 +428,7 @@ function mytreemap(dataset) {
     const height = 445 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
-    const svg = d3.select("#mytreemap")
+    const svg = d3.select("#make-model-treemap")
                   .append("svg")
                   .attr("width", width + margin.left + margin.right)
                   .attr("height", height + margin.top + margin.bottom)
@@ -761,26 +485,26 @@ function mytreemap(dataset) {
        .style("opacity", function(d) { return opacity(d.data.value); });
 
     // and to add the text labels
-    svg.selectAll("text")
-       .data(root.leaves())
-       .enter()
-       .append("text")
-       .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
-       .attr("y", function(d){ return d.y0+20; })    // +20 to adjust position (lower)
-       .text(function(d) { return d.data.name; })
-       .attr("font-size", "12px")
-       .attr("fill", "white");
+    // svg.selectAll("text")
+    //   .data(root.leaves())
+    //   .enter()
+    //   .append("text")
+    //   .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
+    //   .attr("y", function(d){ return d.y0+20; })    // +20 to adjust position (lower)
+    //   .text(function(d) { return d.data.name; })
+    //   .attr("font-size", "12px")
+    //   .attr("fill", "white");
 
     // and to add the text labels
-    svg.selectAll("vals")
-       .data(root.leaves())
-       .enter()
-       .append("text")
-       .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
-       .attr("y", function(d){ return d.y0+35; })    // +20 to adjust position (lower)
-       .text(function(d){ return d.data.value; })
-       .attr("font-size", "11px")
-       .attr("fill", "white")
+    // svg.selectAll("vals")
+    //   .data(root.leaves())
+    //   .enter()
+    //   .append("text")
+    //   .attr("x", function(d){ return d.x0+5; })    // +10 to adjust position (more right)
+    //   .attr("y", function(d){ return d.y0+35; })    // +20 to adjust position (lower)
+    //   .text(function(d){ return d.data.value; })
+    //   .attr("font-size", "11px")
+    //   .attr("fill", "white")
 
     // Add title for the 3 groups
 //    svg.selectAll("titles")
@@ -802,5 +526,188 @@ function mytreemap(dataset) {
 //       .attr("font-size", "19px")
 //       .attr("fill",  "grey" )
 
-// })  
+// })
 }
+  
+  
+// function makeGraphs11(dataset) {
+    
+//     dataset.forEach(function(d) {
+//         d.Units = parseInt(d.Units);
+//     });
+
+//     mybarplot(dataset.filter(function (d) { return d.Year == "2019"; }));
+//     mystackedareaplot(dataset);
+    
+//     // {Rank: "1", Make: "TOYOTA", Units: 21724, Year: "2008"}
+//     //console.log(dataset[0]);
+    
+//     // "1"
+//     //console.log(dataset[0].Rank);
+//     // "TOYOTA"
+//     //console.log(dataset[0].Make);
+//     // 21724
+//     //console.log(dataset[0].Units);
+//     // "2008"
+//     //console.log(dataset[0].Year);
+//     // 489
+//     //console.log(dataset.length);
+// }
+
+// function makeGraphs2(dataset) {
+    
+//     dataset.forEach(function(d) {
+//         d.Units = parseInt(d.Units);
+//         d.Rank = parseInt(d.Rank);
+//         d.Year = parseInt(d.Year);
+//     });
+    
+//     mybumpchart(dataset.filter(function (d) { return (d.Year > 2009); }));
+    
+//     // {Colour: "Grey", Units: 30023, Year: 2019, Rank: 1, Class: "grey"}
+//     //console.log(dataset[0]);
+// }
+
+// function makeGraphs3(dataset) {
+    
+//     // {children: Array(12), name: "UnitsByYearMakeModel"}
+//     //console.log(dataset);
+    
+//     // UnitsByYearMakeModel
+//     //console.log(dataset.name);
+    
+//     // {children: Array(35), name: "2019"}
+//     //console.log(dataset.children[0]);
+    
+//     // "2019"
+//     //console.log(dataset.children[0].name);
+    
+//     // {children: Array(11), name: "VOLVO"}
+//     //console.log(dataset.children[0].children[0]);
+    
+//     // "VOLVO"
+//     //console.log(dataset.children[0].children[0].name);
+    
+//     // {name: "XC60", value: 267}
+//     //console.log(dataset.children[0].children[0].children[0]);
+    
+//     // "XC60"
+//     //console.log(dataset.children[0].children[0].children[0].name);
+//     // 267
+//     //console.log(dataset.children[0].children[0].children[0].value);
+    
+//     mytreemap(dataset.children[0]);
+// }
+
+// /**
+//   * mybarplot()
+//   * 
+//   */
+// function mybarplot(dataset) {
+    
+//     // References:
+//     // https://www.d3-graph-gallery.com/graph/barplot_ordered.html
+//     // https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+    
+//     // set the dimensions and margins of the graph
+//     const margin = {top: 30, right: 30, bottom: 70, left: 60};
+//     const width = 460 - margin.left - margin.right;
+//     const height = 400 - margin.top - margin.bottom;
+
+//     // append the svg object to the div
+//     // add margins
+//     const svg = d3.select("#mybarplot")
+//                   .append("svg")
+//                   .attr("width", width + margin.left + margin.right)
+//                   .attr("height", height + margin.top + margin.bottom)
+//                   .append("g")
+//                   .attr("transform",`translate(${margin.left},${margin.top})`);
+    
+//     // // sort data
+//     // data.sort(function(b, a) {
+//     //     return a.Value - b.Value;
+//     // });
+    
+//     // x-axis scale
+//     var xScale = d3.scaleBand()
+//                   .domain(dataset.map(function(d) { return d.Make; }))
+//                   .range([0, width])
+//                   .padding(0.2);
+    
+//     // append x-axis to the svg object
+//     // translate moves axis to bottom of chart
+//     // this means the axis is actually in the bottom margin
+//     svg.append("g")
+//       .attr("transform", `translate(0, ${height})`)
+//       .call(d3.axisBottom(xScale))
+//       .selectAll("text")
+//       .attr("transform", "translate(-10,0)rotate(-45)")
+//       .style("text-anchor", "end");
+
+//     // y-axis scale
+//     var yScale = d3.scaleLinear()
+//                   .domain([0, d3.max(dataset, function(d) { return d.Units; })])
+//                   .range([height, 0]);
+    
+//     // append y-axis to the svg object
+//     svg.append("g")
+//       .call(d3.axisLeft(yScale));
+
+//     // append the bars
+//     svg.selectAll("rect")
+//       .data(dataset)
+//       .enter()
+//       .append("rect")
+//       .attr("x", function(d) { return xScale(d.Make); })
+//       .attr("y", function(d) { return yScale(d.Units); })
+//       .attr("width", xScale.bandwidth())
+//       .attr("height", function(d) { return height - yScale(d.Units); })
+//       .attr("fill", "#69b3a2");
+    
+//     // add gridlines https://blog.risingstack.com/d3-js-tutorial-bar-charts-with-javascript/
+//     // svg.append('g')
+//     //   .attr('class', 'grid')
+//     //   .attr('transform', `translate(0, ${height})`)
+//     //   .call(d3.axisBottom()
+//     //   .scale(xScale)
+//     //   .tickSize(-height, 0, 0)
+//     //   .tickFormat(''))
+    
+//     svg.append('g')
+//       .attr('class', 'grid')
+//       .call(d3.axisLeft()
+//       .scale(yScale)
+//       .tickSize(-width, 0, 0)
+//       .tickFormat(''));
+       
+//     // add y label
+//     svg.append('text')
+//       .attr('x', -height/2)
+//       .attr('y', -margin.left/2)
+//       .attr('transform', 'rotate(-90)')
+//       .attr('text-anchor', 'middle')
+//       .text('Units');
+    
+//     // add title
+//     svg.append('text')
+//       .attr('x', width / 2 + margin.left)
+//       .attr('y', margin.top/2)
+//       .attr('text-anchor', 'middle')
+//       .text('Car makes by units (2019)');
+    
+//     // add interactivity
+//     svg.selectAll("rect")
+//       .on('mouseenter', function (actual, i) {
+//           console.log(this);
+//           d3.select(this).attr('opacity', 0.5);
+//       })
+//       .on('mouseleave', function (actual, i) {
+//           d3.select(this).attr('opacity', 1);
+//       });
+    
+//     // add colors and fonts
+    
+    
+//     // https://blog.risingstack.com/tutorial-d3-js-calendar-heatmap/
+//     // https://jsfiddle.net/matehu/w7h81xz2/
+// }
